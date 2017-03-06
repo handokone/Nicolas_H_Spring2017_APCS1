2a68d32979b6c950dd56225e7bdc5c6d95d92df6
handokone <18handokon72@Millshighsch.org>
2017-03-05 21:55:03 -0800

    Excell Check One Done

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -3,13 +3,18 @@ package textExcel;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-// Update this file with your own code.
 
 public class TextExcel
 {
 
 	public static void main(String[] args)
 	{
-	    // Add your command loop here
+	    Spreadsheet sheets = new Spreadsheet();
+	    Scanner userInput = new Scanner(System.in);
+	    String userScanner = userInput.next();
+	    while(!userScanner.equals("quit")){
+	    	sheets.processCommand(userScanner);
+	    	userScanner = userInput.next();
+	    }
 	}
 }
