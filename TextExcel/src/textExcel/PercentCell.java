ab99974a566e168e28de324ec0b176663ea135e3
handokone <18handokon72@millshighsch.org>
2017-03-23 08:50:00 -0700

    WHWHWHWWYYYYYYYYYYYYYYY

--- TextExcel/src/textExcel/PercentCell.java
+++ TextExcel/src/textExcel/PercentCell.java
@@ -1,5 +1,14 @@
 package textExcel;
 
 public class PercentCell extends RealCell{
+	
+	public PercentCell(String userInput){
+		super(userInput);
+	}
+	
+	public double getDoubleValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
